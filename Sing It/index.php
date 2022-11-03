c0e423b14ce7156d5ebfb5e76ddc9c425274457d
egasulfikji <egasulfika.2017.7052@gmail.com>
2022-11-04 01:07:11 +0800

    update regis dan login

--- Sing It/index.php
+++ Sing It/index.php
@@ -54,46 +54,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SingIt</title>
-    <link rel="stylesheet" href="stylesheet/style_login.css?v4">
+    <link rel="stylesheet" href="stylesheet/style_login.css?v9">
         
 </head>
 <body >
     <div class = "container">
         <div class = "daftar" >
-        <p class="daftar-text" style="font-size: 2rem; font-weight: 800;">Register</p>                <div class ="bungkus">
-            <form action=""method="POST" class="daftar-user">
-                <div class="input-group">
-                    <input type="text" name="nama" placeholder="Nama" required>
-                </div>
-                <div class="input-group">
-                    <input type="text" name="username" placeholder="Username" required>
-                </div>
-                <div class="input-group">
-                    <input type="password" name="password" placeholder="Password" required>
-                </div>
-                <div class="input-group">
-                    <input type="password" name="password_com" placeholder="Confirmasi Password" required>
-                </div>
-                <button type="submit" name="daftar" class="btn" ><b>Daftar</b></button>
-            </form>
+            <div class ="bungkus">
+                <form action=""method="POST" class="daftar-user">
+                <p class="daftar-text" style="font-size: 2rem; font-weight: 800;">Register</p>    
+                    <div class="input-group">
+                        <input type="text" name="nama" placeholder="Nama" required>
+                    </div>
+                    <div class="input-group">
+                        <input type="text" name="username" placeholder="Username" required>
+                    </div>
+                    <div class="input-group">
+                        <input type="password" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="input-group">
+                        <input type="password" name="password_com" placeholder="Confirmasi Password" required>
+                    </div>
+                    <button type="submit" name="daftar" class="btn" ><b>Daftar</b></button>
+                </form>
             </div>
         </div>
         <div class = "login">
-            <h2>LOGIN</h2>
-            <div class ="form">
-                <form action=""method="POST">
-                    <div class = "bungkus">
-                        <div>
-                            <label for="">Username</label>
-                            <input type="text" name="username" required>
-                        </div>
-                        <div>
-                            <label for="">Password </label>
-                            <input type="password" name="password" required>
-                        </div>
-                        <div>
-                            <button type="submit" name="login"><b>Login</b></button>
-                        </div>
+            <div class ="bungkus">
+                <form action=""method="POST" class="daftar-user">
+                <p class="daftar-text" style="font-size: 2rem; font-weight: 800;">Login</p>
+                    <div class="input-group">
+                        <input type="text" name="username"  placeholder="Username" required>
+                    </div>
+                    <div class="input-group">
+                        <input type="password" name="password" placeholder="Password" required>
+                    </div>
+                    <div>
+                        <button type="submit" name="login" class="btn"><b>Login</b></button>
                     </div>
                 </form>
             </div>
